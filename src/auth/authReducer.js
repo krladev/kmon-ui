baacac5193bcd35743b014d7fe014e611252fb53
Karla Navas <dev@karlanavas.com>
2021-02-23 11:14:17 +1100

    introducing authentication

---
+++ src/auth/authReducer.js
@@ -0,0 +1,19 @@
+import { types } from "../types/types";
+
+export const authReducer = (state = {}, action) => {
+    switch (action.type) {
+        case types.login:
+            return {
+                ...action.payload,
+                auth: true
+            }
+
+        case types.logout:
+            return {
+                auth: false
+            }
+
+        default:
+            return state;
+    }
+}
