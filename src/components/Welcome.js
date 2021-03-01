3b124db49f1b77c5c722dcf47401305c7533a72d
Karla Navas <dev@karlanavas.com>
2021-03-02 00:21:34 +1100

    using challenger engine and removing parent email

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { AuthContext } from '../auth/AuthContext';
+import { initChallenger } from '../engine/initChallenger';
+import { storeChallenger } from '../engine/storeChallenger';
 import { types } from '../types/types';
 
 export const Welcome = ({ history }) => {
@@ -11,35 +13,63 @@ export const Welcome = ({ history }) => {
         setName(e.target.value);
     };
 
-    const [parentEmail, setParentEmail] = useState('');
-    const handleParentEmail = (e) => {
-        setParentEmail(e.target.value);
+    const [numQuestions, setNumQuestions] = useState(10);
+    const handleNumQuestions = (e) => {
+        setNumQuestions(e.target.options[e.target.selectedIndex].value);
     };
 
-    const handleLogin = () => {
-        history.push('/challenger');
+    const [errorMsg, setErrorMsg] = useState('');
 
-        dispatch({
-            type: types.login,
-            payload: {
-                name: name,
-                parentEmail: parentEmail,
-            }
-        })
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        if(name && numQuestions){
+            setErrorMsg('');
+            history.push('/challenger');
+            const minBaseNumber = 4;
+            const challenger = initChallenger({
+                baseNumber: Math.ceil((Math.random() * 5) + minBaseNumber),
+                numQuestions: numQuestions
+            });
+            storeChallenger(challenger);
+
+            dispatch({
+                type: types.login,
+                payload: {
+                    name: name,
+                    numQuestions: numQuestions,
+                    challenger: challenger
+                }
+            });
+        }
+        else{
+            setErrorMsg('Please complete the form');
+        }
     }
 
     return (
         <div className="kui-welcome">
             <h1 className="mb-4">Kumon Challenger</h1>
+            { errorMsg.length ? <div className='alert alert-danger'>{ errorMsg }</div>:"" }
             <form onSubmit={ handleLogin }>
                 <div className="form-group mb-2">
                     <label for="yourName" className="mb-1">Your Name</label>
-                    <input type="text" className="form-control" id="yourName" aria-describedby="Your Name" placeholder="" value={name} onChange={ handleName }/>
+                    <input type="text" className="form-control" id="yourName" aria-describedby="Your Name" placeholder="" value={name} onChange={ handleName } autoComplete="off" />
                 </div>
                 <div className="form-group mb-2">
-                    <label for="parentEmail" className="mb-1">Your Parent's Email</label>
-                    <input type="email" className="form-control" id="parentEmail" aria-describedby="Your Parent's Email" placeholder="" value={parentEmail} onChange={ handleParentEmail } />
+                    <label for="yourName" className="mb-1">Number of questions</label>
+                    <select className="form-control" id="numQuestions" onChange={ handleNumQuestions }>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="30">30</option>
+                        <option value="40">40</option>
+                    </select>
+                    {/* <input type="text" className="form-control" id="yourName" aria-describedby="Your Name" placeholder="" value={name} onChange={ handleName } autoComplete="off" /> */}
                 </div>
+                {/* <div className="form-group mb-2">
+                    <label for="parentEmail" className="mb-1">Your Parent's Email</label>
+                    <input type="email" className="form-control" id="parentEmail" aria-describedby="Your Parent's Email" placeholder="" value={parentEmail} onChange={ handleParentEmail } autoComplete="off" />
+                </div> */}
                 <div className="button-box">
                     <button type="submit" className="btn btn-primary">Start</button>
                 </div>
