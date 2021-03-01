6e67e824a1f615e220ddeb13a68a5ae6af6f57ce
Karla Navas <dev@karlanavas.com>
2021-03-02 00:22:14 +1100

    challenger ui functionality updates and styling

--- src/components/Challenger.js
+++ src/components/Challenger.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 import { FaCheck, FaTimes} from 'react-icons/fa'
 import { AuthContext } from '../auth/AuthContext'
@@ -8,6 +8,13 @@ export const Challenger = ({ history }) => {
 
     const { session, dispatch } = useContext(AuthContext);
 
+    const [challenger, setChallenger] = useState(session.challenger);
+
+    const [userAnswer, setUserAnswer] = useState();
+
+    const question = challenger.questions[challenger.currentQuestion - 1];
+    const answers = challenger.answers[challenger.currentQuestion - 1];
+
     const handleFinish = () => {
         dispatch({
             type: types.logout
@@ -16,14 +23,47 @@ export const Challenger = ({ history }) => {
         history.push('/');
     }
 
+    const recordAnswer = (e) => {
+        const theAnswer = e.target.innerText * 1;
+        setUserAnswer(theAnswer);
+
+        const newChallenger = {...challenger};
+
+        const currentIndex = newChallenger.currentQuestion - 1;
+        newChallenger.userAnswers.push(theAnswer);
+
+        const wasCorrect = newChallenger.correctAnswers[currentIndex] === theAnswer;
+
+        newChallenger.score = wasCorrect
+            ? newChallenger.score + 1
+            : newChallenger.score;
+        newChallenger.currentQuestion++;
+
+        let secondsDelay = 0;
+
+        if(wasCorrect){
+            document.getElementById("audioSuccess").play();
+            secondsDelay = 2;
+        }
+        else{
+            document.getElementById("audioError").play();
+            secondsDelay = 4;
+        }
+
+        window.setTimeout(() => {
+            setChallenger(newChallenger);
+            setUserAnswer(null);
+        }, secondsDelay * 1000);
+    }
+
     return (
-        <div class="kui-challenger">
+        <div className="kui-challenger">
             <div className="row kui-progress">
                 <div className="d-none d-sm-block col-md-6 kui-question-counter">
-                    { session.name }, only 12 questions to go!
+                    { session.name }, only { challenger.questions.length - (challenger.currentQuestion - 1) } questions to go!
                 </div>
                 <div className="col-6 col-lg-5 col-md-4 kui-score">
-                    Score: <span className="kui-score-right">2</span> / <span className="kui-score-wrong">0</span>
+                    Score: <span className="kui-score-right">{ challenger.score }</span> / <span className="kui-score-wrong">{ challenger.questions.length }</span>
                 </div>
                 <div className="col-6 col-lg-1 col-md-2 kui-finish">
                     <button className="nav-item nav-link btn" onClick={ handleFinish }>Finish</button>
@@ -32,17 +72,24 @@ export const Challenger = ({ history }) => {
 
             <div className="kui-qna">
                 <div className="row kui-question">
-                    <div className="col-sm-5 text-center kui-number"><div>8</div></div>
+                    <div className="col-sm-5 text-center kui-number"><div>{ question }</div></div>
                     <div className="col-sm-2 text-center kui-operator"><div>+</div></div>
-                    <div className="col-sm-5 text-center kui-number"><div>6</div></div>
+                    <div className="col-sm-5 text-center kui-number"><div>{ challenger.baseNumber }</div></div>
                 </div>
 
-                <div className="row kui-answers">
-                    <div className="col-6 col-sm-3 text-center kui-answer"><div>22</div></div>
-                    <div className="col-6 col-sm-3 text-center kui-answer"><div>22</div></div>
-                    <div className="col-6 col-sm-3 text-center kui-answer"><div>22</div></div>
-                    <div className="col-6 col-sm-3 text-center kui-answer"><div>22</div></div>
-                </div>
+                {
+                    ! userAnswer ?
+                        <div className="row kui-answers">
+                            <div className="col-6 col-sm-3 text-center kui-answer"><div onClick={ recordAnswer }>{ answers[0] }</div></div>
+                            <div className="col-6 col-sm-3 text-center kui-answer"><div onClick={ recordAnswer }>{ answers[1] }</div></div>
+                            <div className="col-6 col-sm-3 text-center kui-answer"><div onClick={ recordAnswer }>{ answers[2] }</div></div>
+                            <div className="col-6 col-sm-3 text-center kui-answer"><div onClick={ recordAnswer }>{ answers[3] }</div></div>
+                        </div>
+                        :
+                        <div className="row kui-answers">
+                            <div className="col-12 col-sm-3 text-center kui-your-answer"><div>{ userAnswer }</div></div>
+                        </div>
+                }
             </div>
 
             <div className="row d-block d-sm-none kui-progress-bar">
@@ -54,6 +101,13 @@ export const Challenger = ({ history }) => {
             <div className="kui-feedback d-none kui-feedback-cross">
                 <div><FaTimes /></div>
             </div>
+
+            <audio id="audioSuccess">
+                <source src="assets/sounds/Ta Da-SoundBible.com-1884170640.mp3" type="audio/mpeg" />
+            </audio>
+            <audio id="audioError">
+                <source src="assets/sounds/Sad_Trombone-Joe_Lamb-665429450.mp3" type="audio/mpeg" />
+            </audio>
         </div>
     )
 }
