0359066464d852958a9b65b677c9dec98a232e63
Karla Navas <dev@karlanavas.com>
2021-03-02 12:38:09 +1100

    implementing public and private routing

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+  Switch
 } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 
 import { Welcome } from "../components/Welcome";
 import { ChallengerRouter } from "./ChallengerRouter";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
+    const { session } = useContext(AuthContext);
+
     return (
         <Router>
           <div>
@@ -18,8 +21,8 @@ export const AppRouter = () => {
             {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
             <Switch>
-              <Route exact path="/" component={ Welcome } />
-              <Route path="*" component={ ChallengerRouter } />
+              <PublicRoute exact path="/" component={ Welcome } isAuth={ session.auth } />
+              <PrivateRoute path="*" component={ ChallengerRouter } isAuth={ session.auth } />
             </Switch>
           </div>
         </Router>
